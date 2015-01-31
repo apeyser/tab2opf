3d1ceae02b81ff7dd06165b816c99330249047c0
Alex Peyser <peyser.alex@gmail.com>
2015-01-31 22:15:21 +0100

    more dict.cc regex

--- dictcc.py
+++ dictcc.py
@@ -2,13 +2,16 @@ import re
 
 # put the word at the front for the keys
 preps = r"\b(?:mit|an|fur|uber|als|in|zu|von|auf|bis|durch|gegen|ohne|um|aus|auser|bei|gegenuber|nach|seit|entlang|hinter|neben|unter|vor|zwischen|(?:an)?statt|trotz|wahrend|wegen|auserhalb|innerhalb|oberhalb|unterhalb|diesseits|jenseits|beiderseits)\b"
-objs = r"\b(?:(?:sich|etwas|jede[rnms]?)\b|etw\.|jd[rnms]?\.)"
-p = re.compile(r"^(?:\s*(?:{preps}\s+)?{objs})+". \
-               format(preps=preps, objs=objs))
+word = r"[\w\.]+"
+objs = r"\b(?:(?:selbst|sich|etwas|jede[rnms]?)\b|etw\.|jd[rnms]?\.)"
+p = re.compile(r"^(?:\s*(?:{objs}|{preps}\s+{word}))+". \
+               format(preps=preps, word=word, objs=objs))
+pr = re.compile(preps)
 
 # puncts
 puncts = re.compile(r"[,%'\"\\/]")
 per = re.compile(r"(?:\.\.+|\s\.\s)")
+aper = re.compile(r"\.+")
 
 # remove {gender}, [type], (objects)
 extras = r"(?:{[^\}]+}|\[[^\]]*\]|\([^\)]+\))"
@@ -21,16 +24,20 @@ g = re.compile(r"{articles}".format(articles=articles))
 sp = re.compile(r"\s+")
 
 strippers = [
-    [puncts, " "],
-    [per, " "],
-    [e, ''], 
-    [g, ''], 
-    [p, '']
+    [[puncts], " "],
+    [[per], " "],
+    [[e], ''], 
+    [[g], ''], 
+    [[p, aper], ''],
+    [[pr], '']
 ]
 
 def getkey(key):
     for s, r in strippers:
-        nkey = s.sub(r, key).strip()
+        nkey = key
+        for s_ in s:
+            nkey = s_.sub(r, nkey).strip()
+
         if len(nkey) == 0: break
         key = nkey
 

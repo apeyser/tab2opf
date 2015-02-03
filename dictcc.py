7d65821b50d54d0de7ea10bf6101ce146f3395df
Alex Peyser <peyser.alex@gmail.com>
2015-02-03 03:35:22 +0100

    Simplify hyphenated compounds

--- dictcc.py
+++ dictcc.py
@@ -2,32 +2,32 @@ import re
 
 # put the word at the front for the keys
 preps = r"\b(?:mit|an|furs?|ubers?|als|i(?:ns?|m)?|zu[rm]?|vo[nm]|aufs?|bis|durch|gegen|ohne|um|aus|auser|beim?|gegenuber|nach|seit|entlang|hinter|neben|unter|vorm?|zwischen|(?:an)?statt|trotz|wahrend|wegen|auserhalb|innerhalb|oberhalb|unterhalb|diesseits|jenseits|beiderseits)\b"
-word = r"[\w\.]+"
+word = r"\w+"
 objs = r"\b(?:selbst|sich|etwas|jede[rnms]|etw|jd[rnms]?)\b"
 
 p = re.compile(r"\b(?:{objs}|{preps}\s+{word})\b". \
                format(preps=preps, word=word, objs=objs))
-pr = re.compile(preps)
+pr = re.compile(r"(?:^|\s){preps}(?:\s|$)".format(preps=preps))
 
 # remove {gender}, [type], (objects)
 extras = r"(?:{[^\}]+}|\[[^\]]*\]|\([^\)]+\))"
 e = re.compile(extras)
 
 # remove articles
-articles = r"\b(?:d(?:e[rnms]|as|ie)|k?ein(?:e[rnms]?)?)\b"
+articles = r"(?:^|\s)(?:d(?:e[rnms]|as|ie)|k?ein(?:e[rnms]?)?)(?:\s|$)"
 g = re.compile(articles)
 
-sp = re.compile(r"[^\w-]+")
+sp = re.compile(r"\W+")
 ssp = re.compile(r"\s+")
 
 strippers = [
     [
-        [e, ''],   # delete parenthized
+        [e, ' '],   # delete parenthized
         [sp, ' '], # non letters -> space
-        [g, ''],   # delete articles
-        [p, ''],   # delete prep + objects
+        [g, ' '],   # delete articles
+        [p, ' '],   # delete prep + objects
     ], [
-        [pr, ''],  # delete any preps left
+        [pr, ' '],  # delete any preps left
     ], [
         [ssp, ' '], # collapse spaces
     ]
@@ -40,7 +40,7 @@ def getkey(key):
             nkey = s.sub(r, nkey).strip()
             if len(nkey) == 0: break
             key = nkey
- 
+
     return max(key.split(),
                key=lambda k: len(k))
 
